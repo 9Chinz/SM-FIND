7ab958a16ac756b44f8834a19f152b9b09b83bb5
9Chinz <MR.Chinnapat@hotmail.com>
2020-08-27 10:54:32 +0700

    delete php self

--- navbar.php
+++ navbar.php
@@ -5,7 +5,6 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="oi oi-menu"></span> Menu
       </button>
-      <?php echo $_SERVER['PHP_SELF'] ?>
       <div class="collapse navbar-collapse" id="ftco-nav">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item <?php if($_SERVER['PHP_SELF'] == "/SM-FIND/index.php"){ echo "active";} ?>"><a href="index.php" class="nav-link">หน้าแรก</a></li>
