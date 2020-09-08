7912471c8744980bc1746d0b8372ceed1c1b66c4
9Chinz <MR.Chinnapat@hotmail.com>
2020-09-08 12:57:57 +0700

    seperated search bar to other file

---
+++ backend/search_bar.php
@@ -0,0 +1,54 @@
+<div class="row">
+    <div class="col-xl-3 col-md-6 mb-4">
+        <nav class="navbar navbar-expand navbar-light bg-light mb-4">
+            <a class="navbar-brand" href="#">สาขา</a>
+            <select name="dept"class="dropcon">
+                <option value="IT">เทคโนโลยีสารสนเทศ</option>
+                <option value="CG">คอมพิวเตอร์กราฟิก</option>
+                <option value="BC">คอมพิวเตอร์ธุรกิจ</option>
+                <option value="AC">บัญชี</option>
+            </select>
+        </nav>
+    </div>
+    <div class="col-xl-3 col-md-6 mb-4">
+        <nav class="navbar navbar-expand navbar-light bg-light mb-4">
+            <a class="navbar-brand" href="#">ระดับ</a>
+            <select name="section"class="dropcon">
+                <option value="Lower">ปวช</option>
+                <option value="Upper">ปวส</option>
+            </select>
+        </nav>
+    </div>
+    <div class="col-xl-3 col-md-6 mb-4">
+        <nav class="navbar navbar-expand navbar-light bg-light mb-4">
+            <a class="navbar-brand" href="#">ชั้น</a>
+            <select name="class"class="dropcon">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select>
+        </nav>
+    </div>
+    <div class="col-xl-3 col-md-6 mb-4">
+        <nav class="navbar navbar-expand navbar-light bg-light mb-4">
+            <a class="navbar-brand" href="#">ห้อง</a>
+            <select name="room"class="dropcon">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+            </select>
+        </nav>
+    </div>
+</div>
