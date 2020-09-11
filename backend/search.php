db96eddb802ede0ed9f4cb49efdcfd21ee4134d4
9Chinz <MR.Chinnapat@hotmail.com>
2020-09-11 18:30:22 +0700

    add disable option

--- backend/search.php
+++ backend/search.php
@@ -1,6 +1,6 @@
 <?php
 $date = date("Y-m-d");
-if (isset($_POST['btnSearch'])) {
+if (isset($_POST['btnSearch']) AND !empty($_POST['dept']) AND !empty($_POST['section']) AND !empty($_POST['class']) AND !empty($_POST['room'])) {
     $dept = $_POST['dept'];
     $section = $_POST['section'];
     $class = $_POST['class'];
@@ -15,8 +15,10 @@ if (isset($_POST['btnSearch'])) {
     FROM member
     INNER JOIN member_account ON  member.MemberID = member_account.MemberID
     INNER JOIN member_trans ON member_account.AccountID = member_trans.AccountID
-    WHERE member.Dept = '$dept' AND member.Section = '$section' AND member.Class = '$class' AND member.Room = '$room' AND Date = '$date'";
+    WHERE member.Dept = '$dept' AND member.Section = '$section' AND member.Class = '$class' AND member.Room = '$room' AND Date = '$date' AND member_trans.Status = '$status'";
 
     $query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
     $num = mysqli_num_rows($query);
+}else{
+    $num = 0;
 }
