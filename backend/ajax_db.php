572df1b3c1e659061549d1be03dfa18091d019d9
9Chinz <MR.Chinnapat@hotmail.com>
2020-09-11 18:27:39 +0700

    add function query table

--- backend/ajax_db.php
+++ backend/ajax_db.php
@@ -1,5 +1,6 @@
 <?php
 include "../config/connect.php";
+$date = date('Y-m-d');
 
 if(isset($_POST['function']) AND $_POST['function'] == 'degree'){
     $id_degree = $_POST['id'];
@@ -27,5 +28,61 @@ if(isset($_POST['function']) AND $_POST['function'] == 'class'){
     $query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
     while($row = mysqli_fetch_array($query)){
         echo '<option value="'.$row['room'].'">'.$row['room'].'</option>';
+    }
+}
+
+if (isset($_POST['function']) and $_POST['function'] == 'stdSearch') {
+    $id_dept = $_POST['id_dept'];
+    $id_degree = $_POST['id_degree'];
+    $id_class = $_POST['id_class'];
+    $id_room = $_POST['id_room'];
+    $sql = "SELECT * FROM `member` WHERE `Userlevel` = 'student' AND Dept = '$id_dept' AND Section = '$id_degree' AND Class = '$id_class' AND Room = '$id_room'";
+    $query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+    while ($row = mysqli_fetch_array($query)) {
+        $memId = $row['MemberID'];
+        $Accsql = "SELECT * FROM `member` 
+        INNER JOIN member_account on member.MemberID = member_account.MemberID
+        WHERE member.MemberID = '$memId';";
+        $AccQuery = mysqli_query($conn, $Accsql) or die(mysqli_error($conn));
+        $AccRow = mysqli_fetch_array($AccQuery);
+
+        $TransSql = "SELECT * FROM `member` 
+        INNER JOIN member_account on member.MemberID = member_account.MemberID
+        INNER JOIN member_trans on member_account.AccountID = member_trans.AccountID
+        WHERE member.MemberID = '$memId' AND member_trans.Date = '$date';";
+        $TransQuery = mysqli_query($conn, $TransSql) or die(mysqli_error($conn));
+        $TransRow = mysqli_fetch_array($TransQuery);
+
+        if(!empty($AccRow) AND !empty($TransRow)){
+            if($TransRow['Status'] == "5"){
+                $transStatus = "ฝากแล้ว";
+            }else{
+                $transStatus = "รอการยืนยัน";
+            }
+            echo '<tr>
+                <th scope="row">'.$row['MemberCode'].'</th>
+                <td>'.$row['Firstname']." ".$row['Lastname'].'</td>
+                <td>'.$AccRow['Account_number'].'</td>
+                <td>'.$TransRow['Amount'].'</td>
+                <td>'.$transStatus.'</td>
+            </tr>';
+        }else if(empty($TransRow) AND !empty($AccRow)){
+            echo '<tr>
+                <th scope="row">'.$row['MemberCode'].'</th>
+                <td>'.$row['Firstname']." ".$row['Lastname'].'</td>
+                <td>'.$AccRow['Account_number'].'</td>
+                <td>-</td>
+                <td>ยังไม่ฝาก</td>
+            </tr>';
+        }else{
+            echo '<tr>
+                <th scope="row">'.$row['MemberCode'].'</th>
+                <td>'.$row['Firstname']." ".$row['Lastname'].'</td>
+                <td>ไม่มีบัญชี</td>
+                <td>-</td>
+                <td>-</td>
+            </tr>';
+        }
+        
     }
 }
