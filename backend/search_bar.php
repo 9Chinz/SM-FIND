7fccd0a7da79494b527eab148b86436d120ca5f6
9Chinz <MR.Chinnapat@hotmail.com>
2020-09-13 02:36:55 +0700

    add datetimepicker input

--- backend/search_bar.php
+++ backend/search_bar.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-xl-3 col-md-6 mb-4">
-        <nav class="navbar navbar-expand navbar-light bg-light mb-4">
+        <nav class="navbar navbar-expand navbar-light bg-light mb-1">
             <a class="navbar-brand" href="#">สาขา</a>
             <select name="dept" class="dropcon" id="id_dept">
                 <option value="" selected disabled>-เลือกสาขา-</option>
@@ -12,7 +12,7 @@
         </nav>
     </div>
     <div class="col-xl-3 col-md-6 mb-4">
-        <nav class="navbar navbar-expand navbar-light bg-light mb-4">
+        <nav class="navbar navbar-expand navbar-light bg-light mb-1">
             <a class="navbar-brand" href="#">ระดับ</a>
             <select name="section" class="dropcon" id="id_degree">
                 <option value="" selected disabled>-เลือกระดับ-</option>
@@ -22,7 +22,7 @@
         </nav>
     </div>
     <div class="col-xl-3 col-md-6 mb-4">
-        <nav class="navbar navbar-expand navbar-light bg-light mb-4">
+        <nav class="navbar navbar-expand navbar-light bg-light mb-1">
             <a class="navbar-brand" href="#">ชั้น</a>
             <select name="class" id="id_class" class="dropcon">
                 <option value="" selected disabled>-เลือกชั้น-</option>
@@ -30,7 +30,7 @@
         </nav>
     </div>
     <div class="col-xl-3 col-md-6 mb-4">
-        <nav class="navbar navbar-expand navbar-light bg-light mb-4">
+        <nav class="navbar navbar-expand navbar-light bg-light mb-1">
             <a class="navbar-brand" href="#">ห้อง</a>
             <select name="room" id="id_room" class="dropcon">
                 <option value="" selected disabled>-เลือกห้อง-</option>
@@ -38,13 +38,14 @@
         </nav>
     </div>
     <div class="col-xl-3 col-md-6 mb-4">
-        <nav class="navbar navbar-expand navbar-light bg-light mb-4">
-            <a class="navbar-brand" href="#">วัน</a>
+        <nav class="navbar navbar-expand navbar-light bg-light mb-1">
+            <a class="navbar-brand">วัน</a>
             <div class="input-group date" id="datetimepicker4" data-target-input="nearest">
-                    <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker4"/>
-                    <div class="input-group-append" data-target="#datetimepicker4" data-toggle="datetimepicker">
-                        <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                    </div>
+                <input type="text" class="form-control datetimepicker-input" data-toggle="datepicker" id="id_date" >
+                <div class="input-group-append">
+                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                </div>
+            </div>
         </nav>
     </div>
 
